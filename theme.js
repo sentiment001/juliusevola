519906c0f454893888e692e8b476c62e7ae809c6
sentiment001 <mash@hirecmo.io>
2026-09-06 12:53:40 -0400

    Extend Italian language switch to Giorni 1–65

--- theme.js
+++ theme.js
@@ -37,7 +37,7 @@
   var LANGS = [
     { code: 'en', label: 'English', dir: '', days: null, rangeLabel: '' },
     { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(105), rangeLabel: 'Tage 1\u2013105' },
-    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(60), rangeLabel: 'Giorni 1\u201360' }
+    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(65), rangeLabel: 'Giorni 1\u201365' }
   ];
 
   function langByCode(code) {
