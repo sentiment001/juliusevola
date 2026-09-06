2ff9f615de3b29b75e309142f52c3f34b8d98e52
sentiment001 <mash@hirecmo.io>
2026-09-05 21:56:56 -0400

    Open German language switch through Day 75

--- theme.js
+++ theme.js
@@ -36,7 +36,7 @@
 
   var LANGS = [
     { code: 'en', label: 'English', dir: '', days: null, rangeLabel: '' },
-    { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(70), rangeLabel: 'Tage 1\u201370' },
+    { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(75), rangeLabel: 'Tage 1\u201375' },
     { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(30), rangeLabel: 'Giorni 1\u201330' }
   ];
 
