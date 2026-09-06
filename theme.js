0ddfaf96b2af4db0fb564d4539c412713d43a935
sentiment001 <mash@hirecmo.io>
2026-09-06 00:42:58 -0400

    Add Italian Days 51–55 and open Day 50 next-nav

--- theme.js
+++ theme.js
@@ -37,7 +37,7 @@
   var LANGS = [
     { code: 'en', label: 'English', dir: '', days: null, rangeLabel: '' },
     { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(105), rangeLabel: 'Tage 1\u2013105' },
-    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(50), rangeLabel: 'Giorni 1\u201350' }
+    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(55), rangeLabel: 'Giorni 1\u201355' }
   ];
 
   function langByCode(code) {
