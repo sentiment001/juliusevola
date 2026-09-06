7aa498827daa2939f50f57406c36d7c282539c5c
sentiment001 <mash@hirecmo.io>
2026-09-05 23:02:38 -0400

    Extend Italian language switch to Days 1–35

--- theme.js
+++ theme.js
@@ -37,7 +37,7 @@
   var LANGS = [
     { code: 'en', label: 'English', dir: '', days: null, rangeLabel: '' },
     { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(95), rangeLabel: 'Tage 1\u201395' },
-    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(30), rangeLabel: 'Giorni 1\u201330' }
+    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(35), rangeLabel: 'Giorni 1\u201335' }
   ];
 
   function langByCode(code) {
