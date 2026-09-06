222beb587966d9f6aa1e8467d855d70781c1abc1
sentiment001 <mash@hirecmo.io>
2026-09-05 21:30:22 -0400

    Extend German LANGS availability to Days 1–60

--- theme.js
+++ theme.js
@@ -36,7 +36,7 @@
 
   var LANGS = [
     { code: 'en', label: 'English', dir: '', days: null, rangeLabel: '' },
-    { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(55), rangeLabel: 'Tage 1\u201355' },
+    { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(60), rangeLabel: 'Tage 1\u201360' },
     { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(30), rangeLabel: 'Giorni 1\u201330' }
   ];
 
