20c3c1836aaee622b1e99b988861b0c86c9e3136
sentiment001 <mash@hirecmo.io>
2026-09-20 14:40:13 -0400

    Wire it/day-75 next-nav to Day 76; extend theme.js IT range to Giorni 1–80

--- theme.js
+++ theme.js
@@ -54,7 +54,7 @@
   var LANGS = [
     { code: 'en', label: 'English', dir: '', days: null, rangeLabel: '' },
     { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(135), rangeLabel: 'Tage 1\u2013135' },
-    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(75), rangeLabel: 'Giorni 1\u201375' }
+    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(80), rangeLabel: 'Giorni 1\u201380' }
   ];
 
   function langByCode(code) {
