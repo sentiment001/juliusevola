cd28b8571085f23570b087098daf4d956c3eda35
sentiment001 <mash@hirecmo.io>
2026-09-20 22:24:03 -0400

    Extend Italian LANGS range to Days 1–90

--- theme.js
+++ theme.js
@@ -54,7 +54,7 @@
   var LANGS = [
     { code: 'en', label: 'English', dir: '', days: null, rangeLabel: '' },
     { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(135), rangeLabel: 'Tage 1\u2013135' },
-    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(85), rangeLabel: 'Giorni 1\u201385' }
+    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(90), rangeLabel: 'Giorni 1\u201390' }
   ];
 
   function langByCode(code) {
