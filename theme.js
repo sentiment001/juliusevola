a74b87a9f6f742a631be98b6cb0040bbf328eab8
sentiment001 <mash@hirecmo.io>
2026-09-21 23:14:37 -0400

    Extend theme.js Italian range to Giorni 1–105

--- theme.js
+++ theme.js
@@ -54,7 +54,7 @@
   var LANGS = [
     { code: 'en', label: 'English', dir: '', days: null, rangeLabel: '' },
     { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(135), rangeLabel: 'Tage 1\u2013135' },
-    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(100), rangeLabel: 'Giorni 1\u2013100' }
+    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(105), rangeLabel: 'Giorni 1\u2013105' }
   ];
 
   function langByCode(code) {
