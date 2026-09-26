912a74cf03a4a1397f82d37e8f6aa682864ee41a
sentiment001 <mash@hirecmo.io>
2026-09-25 20:44:41 -0400

    Wire it/day-130 next-nav; theme.js IT range Giorni 1-135

--- theme.js
+++ theme.js
@@ -54,7 +54,7 @@
   var LANGS = [
     { code: 'en', label: 'English', dir: '', days: null, rangeLabel: '' },
     { code: 'de', label: 'Deutsch', dir: 'de', days: dayRange(135), rangeLabel: 'Tage 1\u2013135' },
-    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(130), rangeLabel: 'Giorni 1\u2013130' }
+    { code: 'it', label: 'Italiano', dir: 'it', days: dayRange(135), rangeLabel: 'Giorni 1\u2013135' }
   ];
 
   function langByCode(code) {
